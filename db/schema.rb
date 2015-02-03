88f64358b6e8e401b8428fca8e054f11fa1de8d6
Vito Belgiorno-Zegna <bz.vito@gmail.com>
2015-02-03 11:45:31 +1100

    INitial commit on branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131102147) do
+ActiveRecord::Schema.define(version: 20150202013331) do
+
+  create_table "flights", force: :cascade do |t|
+    t.string   "flight_number"
+    t.string   "origin"
+    t.string   "destination"
+    t.datetime "date"
+    t.integer  "plane_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "planes", force: :cascade do |t|
     t.string   "name"
